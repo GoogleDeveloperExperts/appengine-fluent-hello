f036c73839e0334a2a5566af4e7c8d0760de6ba0
David Gageot <david@gageot.net>
2014-11-24 17:11:21 +0100

    Add image

--- java/src/main/java/WebConfiguration.java
+++ java/src/main/java/WebConfiguration.java
@@ -1,17 +1,14 @@
 import net.codestory.http.Configuration;
+import net.codestory.http.filters.log.LogRequestFilter;
 import net.codestory.http.routes.Routes;
 
 public class WebConfiguration implements Configuration {
   @Override
   public void configure(Routes routes) {
-    configureAppEngineRoutes(routes);
-
-    routes.get("/greeting", () -> "Welcome to fluent-http");
-  }
-
-  private void configureAppEngineRoutes(Routes routes) {
     routes
+      .filter(LogRequestFilter.class)
       .get("/_ah/start", "ok")
-      .get("/_ah/health", "ok");
+      .get("/_ah/health", "ok")
+      .get("/greeting", "Welcome to fluent-http");
   }
 }
