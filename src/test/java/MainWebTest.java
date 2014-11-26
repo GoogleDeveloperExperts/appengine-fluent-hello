7e444fbd4ce765423f8210c6c7264225ffd7f755
David Gageot <david@gageot.net>
2014-11-26 10:06:52 +0100

    Layout

--- src/test/java/MainWebTest.java
+++ src/test/java/MainWebTest.java
@@ -3,7 +3,7 @@ import net.codestory.simplelenium.SeleniumTest;
 import org.junit.Test;
 
 public class MainWebTest extends SeleniumTest {
-  WebServer webServer = new WebServer().configure(WebConfiguration.class).startOnRandomPort();
+  WebServer webServer = new WebServer().configure(MainWeb.WebConfiguration.class).startOnRandomPort();
 
   @Override
   protected String getDefaultBaseUrl() {
@@ -14,7 +14,8 @@ public class MainWebTest extends SeleniumTest {
   public void index() {
     goTo("/");
 
-    find("h1").should().contain("Welcome to fluent-http");
+    find("h1").should().contain("Hello World!");
+    find("h3").should().contain("Welcome to fluent-http");
     find("p").should().contain("Brought to you by Google App Engine.");
   }
 }
